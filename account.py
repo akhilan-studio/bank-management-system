355e449a87791941cd8f1567c6004d43e265ed10
Bala <Akhilan.studios@gmail.com>
2025-10-22 13:24:19 +0530

    parameterised and minor fixes for tranfer

--- account.py
+++ account.py
@@ -17,12 +17,7 @@ def show_account(root,accno):
     ttk.Button(frm, text="Deposit", command=lambda: (acc.destroy(),show_deposit(acc,accno,root))).grid(column=0, row=1)
     ttk.Button(frm, text="Withdraw", command=lambda: (acc.destroy(),show_withdraw(root,accno))).grid(column=1, row=1)
     ttk.Button(frm, text="Transfer", command=lambda: (acc.destroy(),show_transfer(root,accno))).grid(column=0, row=2, columnspan=2)
-    ttk.Button(frm, text="Logout", command=lambda: logout(acc, root)).grid(column=0, row=3, columnspan=2, pady=10)
-    
-
-def logout(account_win, root):
-    account_win.destroy()
-    root.deiconify()
+    ttk.Button(frm, text="Logout", command=lambda: (acc.destroy(),root.deiconify())).grid(column=0, row=3, columnspan=2, pady=10)
 
 
 
@@ -47,6 +42,7 @@ def depocheck(acc,bal):
         messagebox.showerror('Invalid ammount','entered amount should number')
 
 
+
 def show_transfer(root,accno):
     tf = Tk()
     tf.title("Transfer")
@@ -56,27 +52,19 @@ def show_transfer(root,accno):
     ttk.Label(frm, text="Recipient Acc No:").grid(column=0, row=0)
     accnoto=StringVar(tf)
     ttk.Entry(frm,textvariable=accnoto).grid(column=1, row=0)
+    
     ttk.Label(frm, text="Enter amount:").grid(column=0, row=1)
     amt=StringVar(tf)
     ttk.Entry(frm,textvariable=amt).grid(column=1, row=1)
 
-    ttk.Button(frm, text="Confirm",command=lambda: (tf.destroy(),transfer_check(accno,accnoto.get(),amt.get()),show_account(root, accno))).grid(column=0, row=2, columnspan=2, pady=5)
+    ttk.Button(frm, text="Confirm",command=lambda: (transfer_check(accno,accnoto.get(),amt.get(),tf,root))).grid(column=0, row=2, columnspan=2, pady=5)
     ttk.Button(frm, text="Back",command=lambda: (tf.destroy(), show_account(root,accno))).grid(column=0, row=3, columnspan=2)
 
-def transfer_check(accnofr,accnoto,amt):
-    if amt.isdigit():
-        amt=int(amt)
-        if m.balance(accnofr)<amt:
-            messagebox.showerror('Invalid amount','you do not have enough balance')
-        elif amt<0:
-            messagebox.showerror('Invalid ammount','entered amount cannot be negative')
-        elif m.balance(accnoto)==None:
-            messagebox.showerror('Invalid account','account does not exist')
-        else:
-            m.Transfer(accnofr,accnoto,amt)
+def transfer_check(accnofr,accnoto,amt,tf,root):
+           if m.Transfer(accnofr,accnoto,amt):
+               tf.destroy()
+               show_account(root, accnofr)
     
-    else:
-        messagebox.showerror('invalid value','pls enter a number')
 def show_withdraw(root,acc):
     wd = Tk()
     wd.title("Withdraw")
--- mysql_connection.py
+++ mysql_connection.py
@@ -9,43 +9,51 @@ my=m.connect(host='localhost',user='root',passwd='bloodgod@609',database='bank')
 
 def Transfer(accnofr,accnoto,amt):
     global my
-    cu1=my.cursor()
-    ex='update main set balance={new} where accno={acc};'
-    amtfr=balance(accnofr)-amt
-    cu1.execute(ex.format(new=amtfr,acc=accnofr))
-    my.commit()
-    amtto=balance(accnoto)+amt
-    cu1.execute(ex.format(new=amtto,acc=accnoto))
-    my.commit()
-    cu1.close()
-    cu2=my.cursor()
-    ex='insert into log values({accno},"Transfer",{amount},{taccno},sysdate())'
-    cu2.execute(ex.format(accno=accnofr,amount=amt,taccno=accnoto))
-    my.commit()
+    success=False
+    if amt.isdigit()==False:
+        messagebox.showerror('Invalid amount','value is wrong')
+    elif balance(accnofr)<int(amt):
+        messagebox.showerror('Invalid amount','you do not have enough balance')
+    elif int(amt)<0:
+        messagebox.showerror('Invalid ammount','entered amount cannot be negative')
+    elif balance(accnoto)==None:
+        messagebox.showerror('Invalid account','account does not exist')
+    elif accnoto==accnofr:
+        messagebox.showerror('invalid input',"can't transfer to self")
+    else:
+        amt=int(amt)
+        cu1=my.cursor()
+        amtfr=balance(accnofr)-amt
+        amtto=balance(accnoto)+amt
+        ex='update main set balance=%s where accno=%s;'
+        cu1.execute(ex,(amtfr,accnofr))
+        cu1.execute(ex,(amtto,accnoto))
+        ex='insert into log values(%s,"Transfer",%s,%s,sysdate())'
+        cu1.execute(ex,(accnofr,amt,accnoto))
+        my.commit()
+        cu1.close()
+        success=True
+    return success
+        
     
 def edit_balance(acc,bal):
     global my
     cu1=my.cursor()
-    ex='update main set balance={new} where accno={acc};'
+    ex='update main set balance=%s where accno=%s;'
     new=balance(acc)+int(bal)
-    cu1.execute(ex.format(new=new,acc=acc))
+    cu1.execute(ex,(new,acc))
+    if bal[0]=='-':
+        cu1.execute('insert into log values(%s,"withdraw",%s,Null,sysdate())',(acc,bal))
+    else:
+        cu1.execute('insert into log values(%s,"deposit",%s,Null,sysdate())',(acc,bal))
     my.commit()
     cu1.close()
- 
-    cu2=my.cursor()
-    if bal[0]=='-':
-        cu2.execute('insert into log values({accno},"withdraw",{amount},Null,sysdate())'.format(accno=acc,amount=bal))
-    else:
-        cu2.execute('insert into log values({accno},"deposit",{amount},Null,sysdate())'.format(accno=acc,amount=bal))
-    cu2.close()
-    my.commit()
-
 
 def balance(accno):
     global my
     cu1=my.cursor()
-    ex="select balance from main where accno={accno};"
-    cu1.execute(ex.format(accno=accno))
+    ex="select balance from main where accno=%s;"
+    cu1.execute(ex,(accno))
     data=cu1.fetchone()
     cu1.close()
     if data!=None:
@@ -57,43 +65,35 @@ def loginchecker(passwd,accno,root):
     root.withdraw()
     if accno.isdigit():
         accno=int(accno)
+        cu1=my.cursor()
+        ex="select accno,password from main where accno=%s and password=%s;"
+        cu1.execute(ex,(accno,passwd))
+        data=cu1.fetchone()
+        cu1.close()
+        if data:
+            account.show_account(root,accno)
+        else:
+            messagebox.showerror("Login Failed", "Invalid account number or password")
+            root.deiconify()
     else:
         messagebox.showerror('invalid value','enter an account no')
-    cu1=my.cursor()
-    ex="select accno,password from main where accno={accno} and password='{passwd}';"
-    cu1.execute(ex.format(accno=accno,passwd=passwd))
-    data=cu1.fetchone()
-    cu1.close()
-    if data:
-        account.show_account(root,accno)
-    else:
-        messagebox.showerror("Login Failed", "Invalid account number or password")
-        root.deiconify()
 
 def Accgen(name,DOB,passwd,root,nu):
     global my
     cu1=my.cursor()
     #no two same account no presence logic
     while True:
-        x=random.randint(100,999)
-        ex='select accno from main where accno='+str(x)+';'
-        cu1.execute(ex)
+        accno=random.randint(100,999)
+        ex='select accno from main where accno=%s;'
+        cu1.execute(ex,(x))
         if cu1.fetchall()==[]:
             break
-    cu1.close()
     try:
-        #adding account to new table
-        ex="insert into main values({x},'{name}','{DOB}','{passwd}',0);"
-        cu2=my.cursor()
-        cu2.execute(ex.format(x=x,name=name,DOB=DOB,passwd=passwd))
+        ex="insert into main values(%s,%s,%s,%s,0);"
+        cu1.execute(ex,(accno,name,DOB,passwd))
+        cu1.execute('insert into log values(%s,"account created",Null,Null,sysdate())',(accno))
         my.commit()
-        cu2.close()
-        
-        #adding to log
-        cu2=my.cursor()
-        cu2.execute('insert into log values({accno},"account created",Null,Null,sysdate())'.format(accno=x))
-        my.commit()
-        cu2.close()
+        cu1.close()
         
         #showing account number
         nu.destroy()
